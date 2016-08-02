c217d625a76bf4560bc472f8d4e83e34828695c9
Daniel Yim <danielyim@gmail.com>
2016-08-02 16:05:41 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# hyperterm-zenburn
+
+# Zenburn theme for Hyperterm
+
+A [Hyperterm](https://hyperterm.org/) theme based on the [Zenburn](http://kippura.org/zenburnpage/) theme by Jani Nurminen.
+
+### Installation
+1. Open HyperTerm's preferences by pressing `Cmd+,` or manually at `~/.hyperterm.js`.
+2. Add `hyperterm-zenburn2` to the array of hyperterm plugins.
+
+  ```js
+  plugins: [
+    'hyperterm-zenburn'
+  ],
+  ```
+3. Reload HyperTerm by pressing `Cmd + Shft + R`
+
+### License
+MIT
---
+++ index.js
@@ -0,0 +1,40 @@
+const backgroundColor = '#3d3d3d'
+const borderColor = '#777'
+const foregroundColor = 'rgba(239,240,165,0.75)'
+const cursorColor = '#657B83'
+
+const colors = [
+  '#505050', // Black
+  '#d7a5a4', // Red
+  '#91ad91', // Green
+  '#e8d7b9', // Yellow
+  '#c6eaff', // Blue
+  '#ba90bd', // Magenta
+  '#9ad6d9', // Cyan
+  '#f3f1d9', // White
+  '#7f7f7f', // Bright Black
+  '#e4b3b2', // Bright Red
+  '#d4e894', // Bright Green
+  '#eff0a5', // Bright Yellow
+  '#6996bf', // Bright Blue
+  '#dcbecd', // Bright Magenta
+  '#9ad6d9', // Bright Cyan
+  '#f8f5ed'  // Bright White
+]
+
+exports.decorateConfig = config => {
+
+  return Object.assign({}, config, {
+    foregroundColor,
+    backgroundColor,
+    borderColor,
+    cursorColor,
+    colors,
+    css: `
+      ${config.css || ''}
+      .tab_active:before {
+        border-bottom-color: ${backgroundColor};
+      }
+    `
+  })
+}
